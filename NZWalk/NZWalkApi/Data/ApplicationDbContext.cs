f6a7843831ad3b3a92669c30b453ce7f99f97770
pc-Pandey <impc.pandey@gmail.com>
2023-02-14 00:23:02 +0530

    First Commit

---
+++ NZWalk/NZWalkApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalkApi.Models.Domain;
+
+namespace NZWalkApi.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
